18c22111e610a0e6f1d44bef84685637ecdc0ca3
maxSanchez <slemuh.ms@gmail.com>
2016-10-29 03:09:19 -0500

    Agreguen sus nombres primero que todo

---
+++ lp/programa7.java
@@ -0,0 +1,35 @@
+/*
+Programa 7 
+
+
+DESCRIPCION:
+ En el lenguaje OO de tu preferencia, desarrolla los codigos necesarios para mostrar:
+ -la forma en la que se realiza la eleccion y el encadenamiento de constructores
+ -el uso de los diferentes tipos de vista que provee
+ -la forma en la que se realiza la busqueda dinamica de metodos
+Nota: Debes dar una breve explicacion de cada concepto, basandote en los codigos y resultados.
+
+EQUIPO:
+
+-Sanchez Hernandez Max Armando
+
+MATERIA: Lenguajes de programacion
+
+SEMESTRE: 2017-1
+
+
+*/
+
+public class programa7 {
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+}
+
+public class Vista{
+	private String textoPrivado="Hola este es un texto privado";
+	public String textoPublico="Hola este es un texto publico";
+
+	public static 
+
+}
